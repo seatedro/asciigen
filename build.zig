77e243bc3c1a92a807f4d890fea9df14ded3ad95
ro <54208873+rohitp934@users.noreply.github.com>
2024-08-27 14:48:27 -0400

    let's see if this works

--- build.zig
+++ build.zig
@@ -2,9 +2,9 @@ const std = @import("std");
 
 const targets: []const std.Target.Query = &.{
     .{ .cpu_arch = .aarch64, .os_tag = .macos },
+    .{ .cpu_arch = .x86_64, .os_tag = .macos },
     .{ .cpu_arch = .aarch64, .os_tag = .linux },
-    .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .gnu },
-    .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .musl },
+    .{ .cpu_arch = .x86_64, .os_tag = .linux },
     .{ .cpu_arch = .x86_64, .os_tag = .windows },
 };
 pub fn build(b: *std.Build) !void {
