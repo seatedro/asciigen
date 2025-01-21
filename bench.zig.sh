386bfd2e1d6c2e85474540b4e169bbae26c607db
Rui Chen <rui@chenrui.dev>
2025-01-21 12:16:08 -0500

    update build flags
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- bench.zig.sh
+++ bench.zig.sh
@@ -1,2 +1,2 @@
-# zig build run -Drelease -- -i images/skull.png -o ascii.png -e -c -b 15.0
-zig build run -Drelease -- -i "https://w.wallhaven.cc/full/85/wallhaven-856dlk.png" -o ascii.png -e -c --full_characters --sorted_ovr
+# zig build run --release=fast -- -i images/skull.png -o ascii.png -e -c -b 15.0
+zig build run --release=fast -- -i "https://w.wallhaven.cc/full/85/wallhaven-856dlk.png" -o ascii.png -e -c --full_characters --sorted_ovr
